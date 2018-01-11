db97514f32900014fead797d4ad1f758f71583e6
pawanandro <pkpawan0110@gmail.com>
2018-01-11 18:45:21 +0530

    first commit with login and connections

---
+++ app/src/main/java/switzersearch/com/switzertrainsearch/SplashActivity.java
@@ -0,0 +1,42 @@
+package switzersearch.com.switzertrainsearch;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/zag_bold.otf");
+        TextView tv1 = (TextView) findViewById(R.id.tv_sts);
+        tv1.setTypeface(face);
+
+        goToNextActivity();
+    }
+
+    private void goToNextActivity() {
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                {
+                    Log.d("isFirstTimeUser", "true:");
+
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+
+                }
+            }
+        }, 5000);
+    }
+
+}
